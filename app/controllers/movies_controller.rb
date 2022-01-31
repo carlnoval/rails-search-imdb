7b6f580776e87f07a59d2a668418e926d4a99940
Carl Noval <cjrnoval22@gmail.com>
2022-01-31 23:38:51 +0900

    basic search, case sensitive and case insensitive

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,15 @@
 # rails g controller movies
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+    if params[:query].present?
+
+      # only returns movie title with case sensitive exact match
+      # @movies = Movie.where(title: params[:query])
+
+      # the `I` before `LIKE` takes care of exact case matching
+      @movies = Movie.where("title ILIKE ?", params[:query])
+    else
+      @movies = Movie.order(title: :asc)
+    end
   end
 end
