8684c8e9d228561562510d6a5783c22d85617a75
Carl Noval <cjrnoval22@gmail.com>
2022-02-01 00:14:33 +0900

    serach with related table

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,14 +12,25 @@ class MoviesController < ApplicationController
       # takes care of also searching for entered searched item in the synopsis
       # Security NOTE: this query still prevents sql injection, the usual `?` on preventing sql injections just got replaced by :query
       # Search NOTE: this does not take care of searching for `batman v superman` using only `batman superman`
-      @movies = Movie.where("title ILIKE :query OR synopsis ILIKE :query", query: "%#{params[:query]}%")
+      # @movies = Movie.where("title ILIKE :query OR synopsis ILIKE :query", query: "%#{params[:query]}%")
 
       # @@ is postgresSQL's full text search - searching for `jump` will also search for any words associated to jump like jumped, jumping, etc
       # @@ applies above method on each word entered on the search field
       # allows to search for `batman v superman` using these searches: `batman superman`, `superman batman`
       # fears will allow search match for `batman v superman` cause synopsis has word `fear`
       # Search NOTE: this setup won't work with partial search, eg `batman sup` won't search for `batman v superman`
-      @movies = Movie.where("title @@ :query OR synopsis @@ :query", query: "%#{params[:query]}%")
+      # Search NOTE: this won't be able to search via movie directors, `nolan` won't yeild any results
+      # @movies = Movie.where("title @@ :query OR synopsis @@ :query", query: "%#{params[:query]}%")
+
+      # this setup allows to search for movie directors
+      # unsure about the `\` character, could be just for convention but may also break something if removed
+      sql_query = " \
+        movies.title @@ :query \
+        OR movies.synopsis @@ :query \
+        OR directors.first_name @@ :query \
+        OR directors.last_name @@ :query \
+      "
+      @movies = Movie.joins(:director).where(sql_query, query: "%#{params[:query]}%")
     else
       @movies = Movie.order(title: :asc)
     end
